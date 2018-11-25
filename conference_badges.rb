f1d58d0ff45404430d6c60d15836924e8b1b1c47
Stephanie Messad <sabud1208@gmail.com>
2018-11-25 17:09:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-    attendees.each_with_index.map do |room|
+    attendees.each_with_index.map do |room, index|
   room_assignments = "Hello, #{room}!, You'll be assigned to room #{attendees.length}!"
   room_assignments
 end
