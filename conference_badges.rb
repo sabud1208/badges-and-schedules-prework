55a121dac3ac606dce2fea137cbec75f6200f32d
Stephanie Messad <sabud1208@gmail.com>
2018-11-25 16:02:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+badge_maker("Stephanie")
+
+speakers= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+  badges = speakers.map do |badge|
+    puts "Hello, my name is #{badge.chomp}."
+  end
+end
+
+def assign_rooms(speakers)
+      num=0
+while num < speakers.length do
+  puts "Hello #{speakers[num]}, your room number is #{num+1}. "
+   num += 1
+   end
+end
+
+def printer(speakers)
+  puts batch_badge_creator(speakers)
+  puts assign_rooms(speakers)
+end
